c106f93516cdcbe02140a4e3b596b4199229f2c1
Nicolas Burniske <nicoburniske@gmail.com>
2020-01-12 17:26:56 -0500

    added unit tests that don't involve parsing

---
+++ 1/test/STest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class STest {
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void testCount() {
+
+    }
+
+    @Test
+    public void testReplace() {
+
+    }
+
+
+}
