005bcbdb0ada3387e3927d7472fdb7b01a529570
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-01-17 12:06:08 -0500

    berzberg undo

--- 2/Other/src/main/java/AccumulatorType.java
+++ 2/Other/src/main/java/AccumulatorType.java
@@ -1,9 +1,8 @@
 public class AccumulatorType {
     Integer width;
     Integer depth;
-    String val;
 
-    public AccumulatorType(String val, Integer depth, Integer width) {
+    public AccumulatorType(Integer depth, Integer width) {
         this.width = width;
         this.depth = depth;
     }
