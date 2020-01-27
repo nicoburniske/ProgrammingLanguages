dc28178ecb64274bea42fd719fb08a6d9fe2265f
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-01-27 13:12:48 -0500

    bherzberg making main work

--- 3/Other/src/main/java/Main.java
+++ 3/Other/src/main/java/Main.java
@@ -16,26 +16,25 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws ParseException, IOException, LispParser.ParseException {
-//        Object obj = new JSONParser().parse(new FileReader(args[1]));
-//        VExpr result = parse(obj);
-//        if ("sd".equals(args[0])) {
-//            System.out.println(result.sd(new HashMap<String, Stack<AccumulatorType>>(), 0).toJson());
-//        } else if ("interpreter".equals(args[0])) {
-//            try {
-//                //TODO: figure out the size
-//                int val = result.evaluate(new StackList<StackList<Integer>>(result.getMaxNumberOfScopedVariables()));
-//                System.out.println(val);
-//            } catch (IllegalStateException e) {
-//                System.out.println(e.getMessage());
-//            }
-//        } else {
-//            throw new IllegalArgumentException("Error: an illegal function was requested");
-//        }
-        LispTokenizer tzr = new LispTokenizer(
-                "((let x = 2) (x + 3))");
-        LispParser parser = new LispParser(tzr);
-        LispParser.Expr result = parser.parseExpr();
-        System.out.println(parseSVexp(result).toJson());
+
+        if ("ast".equals(args[0])) {
+            LispTokenizer tzr = new LispTokenizer(new FileReader(args[1]));
+            LispParser parser = new LispParser(tzr);
+            LispParser.Expr result = parser.parseExpr();
+            System.out.println(parseSVexp(result).toJson());
+        } else if ("interpreter".equals(args[0])) {
+            Object obj = new JSONParser().parse(new FileReader(args[1]));
+            VExpr result = parse(obj);
+            try {
+                //TODO: figure out the size
+                int val = result.evaluate(new StackList<StackList<Integer>>(result.getMaxNumberOfScopedVariables()));
+                System.out.println(val);
+            } catch (IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            throw new IllegalArgumentException("Error: an illegal function was requested");
+        }
     }
 
     private static VExpr parse(Object obj) {
