e675df9ea0cd3327d79c6fd455910e1e3f633dc5
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-02-03 12:36:47 -0500

    bherzberg added execution to main

--- 4/Other/src/main/java/Main.java
+++ 4/Other/src/main/java/Main.java
@@ -1,10 +1,13 @@
+import answer.Answer;
 import fvexpr.FVExpr;
+import fvexpr.Var;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import parser.ParseUtils;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class Main {
@@ -13,11 +16,7 @@ public class Main {
          if ("interpreter".equals(args[0])) {
             Object obj = new JSONParser().parse(new FileReader(args[1]));
             FVExpr result = ParseUtils.parse(obj);
-            try {
-
-            } catch (IllegalStateException e) {
-                System.out.println(e.getMessage());
-            }
+            result.interpret(new HashMap<Var, Answer>());
         } else {
             throw new IllegalArgumentException("Error: an illegal function was requested");
         }
