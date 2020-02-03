90f1e413712f6d77fd4fb242591e6adcf6dcb059
Nicolas Burniske <nicoburniske@gmail.com>
2020-02-03 16:44:49 -0500

    fixed exponent arithmetic and added tests

--- 4/Other/src/main/java/Main.java
+++ 4/Other/src/main/java/Main.java
@@ -55,7 +55,7 @@ public class Main {
         stdLib.put(new Var("^"), new AnswerFunction(new Func(Arrays.asList(new Var("left"), new Var("right")), new FVExpr() {
             @Override
             public Answer interpret(HashMap<Var, Answer> acc) {
-                return acc.get(new Var("left")).pow(acc.get(new Var("right")));
+                return (acc.get(new Var("right")).pow(acc.get(new Var("left"))));
             }
 
             @Override
