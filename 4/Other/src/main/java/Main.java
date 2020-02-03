0b1d9d735826043a6bebff1156ed86eaa1190a3f
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-02-03 14:51:01 -0500

    bherzberg my attempt

--- 4/Other/src/main/java/Main.java
+++ 4/Other/src/main/java/Main.java
@@ -1,8 +1,9 @@
 import answer.Answer;
 import answer.AnswerFunction;
-import fvexpr.BinaryFunction;
+import answer.AnswerInt;
 import fvexpr.FVExpr;
 import fvexpr.Func;
+import fvexpr.Operator;
 import fvexpr.Var;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -10,6 +11,8 @@ import parser.ParseUtils;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -19,19 +22,18 @@ public class Main {
          if ("interpreter".equals(args[0])) {
             Object obj = new JSONParser().parse(new FileReader(args[1]));
             FVExpr result = ParseUtils.parse(obj);
-             HashMap<String, BinaryFunction> stdLib = new HashMap<String, BinaryFunction>();
-             stdLib.put("+", new BinaryFunction(){
+             HashMap<Var, Answer> stdLib = new HashMap<Var, Answer>();
+             stdLib.put(new Var("+"), new AnswerFunction(new Func(Arrays.asList(new Var("left"), new Var("right")), new FVExpr() {
+                         @Override
+                         public Answer interpret(HashMap<Var, Answer> acc) {
+                             return new AnswerInt(((BigInteger)acc.get(new Var("left")).result).add((BigInteger)acc.get(new Var("right")).result));
+                         }
 
-                 @Override
-                 public Answer apply(Answer left, Answer right) {
-                     return null;
-                 }
-
-                 @Override
-                 public String toJson() {
-                     return null;
-                 }
-             });
+                         @Override
+                         public String toJson() {
+                             return null;
+                         }
+                     })));
             System.out.println(result.interpret(new HashMap<Var, Answer>()).result);
         } else {
             throw new IllegalArgumentException("Error: an illegal function was requested");
