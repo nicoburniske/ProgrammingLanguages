0e2db91bc1771e402f6399520332f35564736ad5
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-02-06 15:01:47 -0500

    fixed the constants

--- 5/Other/src/main/java/fvexpr/Constants.java
+++ 5/Other/src/main/java/fvexpr/Constants.java
@@ -7,6 +7,6 @@ public interface Constants {
     String ERROR_UNDECLARED_VARIABLE_TEMPLATE = "\"variable %s undeclared\"";
     String ERROR_INVALID_ARITHMETIC = "\"arithmetic error\"";
     String ERROR_CLOSURE_EXPECTED = "\"closure or primop expected\"";
-    String ERROR_ARGUMENTS_MISMATCH = "\"number of arguments does not match the number of parameters\"";
+    String ERROR_ARGUMENTS_MISMATCH = "\"number of arguments does not match number of parameters\"";
     String CLOSURE_STRING = "\"closure\"";
 }
