0a1f8765181d029e6c9309cbe93e4c1ba15784e2
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-02-15 15:11:39 -0500

    missing a JSON class

--- 6/Other/src/main/java/tast/star_decl/StarDecl.java
+++ 6/Other/src/main/java/tast/star_decl/StarDecl.java
@@ -1,17 +1,29 @@
 package tast.star_decl;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
 import tast.star_ast.StarAST;
 import tast.TASTVar;
 
 /**
  * TODO: DATA Definition
  */
-public class StarDecl {
+public class StarDecl implements JSONAware {
     TASTVar name;
     StarAST rhs;
 
     public StarDecl(TASTVar name, StarAST rhs) {
         this.name = name;
         this.rhs = rhs;
+    }
+
+    @Override
+    public String toJSONString() {
+        JSONArray arr = new JSONArray();
+        arr.add("let");
+        arr.add(name);
+        arr.add("=");
+        arr.add(rhs);
+        return arr.toJSONString();
     }
 }
