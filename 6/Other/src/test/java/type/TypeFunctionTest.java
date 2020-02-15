fa06fab3c1be14cac5872fff7927c2f5978a5244
Nick Burniske <nicoburniske@gmail.com>
2020-02-15 15:38:36 -0500

    fixed imports

--- 6/Other/src/test/java/type/TypeFunctionTest.java
+++ 6/Other/src/test/java/type/TypeFunctionTest.java
@@ -1,15 +1,18 @@
 package type;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
 
 class TypeFunctionTest {
 
     @Test
     void toJSONString() {
         assertEquals(new TypeFunction(Arrays.asList(new TypeInt(), new TypeInt(), new TypeInt()), new TypeInt()).toJSONString(), "[\"int\",\"int\",\"int\",\"->\",\"int\"]" );
+        assertEquals(new TypeFunction(Arrays.asList(new TypeInt(), new TypeInt(), new TypeInt()), new TypeFunction(Arrays.asList(new TypeInt()), new TypeInt())).toJSONString(), "[\"int\",\"int\",\"int\",\"->\",[\"int\",\"->\",\"int\"]]");
     }
 }
