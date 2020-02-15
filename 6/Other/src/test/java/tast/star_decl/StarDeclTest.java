6c5feed63c126aecdadd63c2f3d916701fe8a64e
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-02-15 15:40:08 -0500

    bherzberg fixing test

--- 6/Other/src/test/java/tast/star_decl/StarDeclTest.java
+++ 6/Other/src/test/java/tast/star_decl/StarDeclTest.java
@@ -1,17 +1,26 @@
 package tast.star_decl;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+import tast.TASTVar;
+import tast.star_ast.StarAST;
+import type.TypeFunction;
+import type.TypeInt;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 class StarDeclTest {
 
-    @BeforeEach
+    @Before
     void setUp() {
     }
 
     @Test
     void toJSONString() {
+        StarDecl starDecl  = new StarDecl(new TASTVar("dec"),new StarAST(new TASTVar("les"),new TypeFunction(Arrays.asList(new TypeInt(), new TypeInt()), new TypeInt())));
+        assertEquals("", starDecl.toJSONString());
     }
 }
