39e0e1b308b0922395049c424a2232f54b4d3ba3
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-03-12 11:14:24 -0400

    moved everything to classes

--- 8/Other/src/main/java/typechecker/tast/TASTDeclArray.java
+++ 8/Other/src/main/java/typechecker/tast/TASTDeclArray.java
@@ -6,6 +6,7 @@ import typechecker.tast.star_decl.StarDecl;
 import typechecker.type.Type;
 import typechecker.type.TypeFunction;
 import typechecker.type.TypeInt;
+import typechecker.type.TypedVar;
 
 import java.util.List;
 import java.util.Objects;
@@ -61,6 +62,34 @@ public class TASTDeclArray implements TAST {
             typeDecl = String.format("Supplier<%s>", this.rhs.getType().toJava());
         }
         String result = this.rhs.toJava();
-        return String.format("((%s)%s -> {\n%s%sreturn %s;\n})%s",typeDecl, inputs, functionNames,functionAssignments, result, applies);
+        List<Type> overallType = integers.stream().map(curr -> curr.name.getType()).collect(Collectors.toList());
+        return  toJavaHelper(integers.stream().map(curr -> curr.name).collect(Collectors.toList()), new TypeFunction(overallType, type), functionNames, functionAssignments);
+        //return String.format("(new %s() {\n @Override\npublic %s %s {%s%sreturn %s;\n}})%s",typeDecl, inputs, functionNames,functionAssignments, result, applies);
+    }
+
+    private String toJavaHelper(List<TypedVar> params, Type type, String functionNames, String functionAssignments) {
+        if(params.size() > 1) {
+            TypeFunction functionType = (TypeFunction) type;
+            Type returnType = functionType.removeOneArg();
+            return String.format("(new %s() {\n@Override\n" +
+                            "            public %s apply(%s) {\nreturn %s;\n}\n})",
+                    functionType.toJava(),
+                    returnType.toJava(),
+                    params.get(0).toJava(),
+                    toJavaHelper(
+                            params.subList(0, params.size()),
+                            returnType,functionNames, functionAssignments));
+        } else {
+            TypeFunction functionType = (TypeFunction) type;
+            Type returnType = functionType.removeOneArg();
+            return String.format("(new %s() {\n@Override\n" +
+                            "            public %s apply(%s) {%s%sreturn %s;\n}\n})",
+                    functionType.toJava(),
+                    returnType.toJava(),
+                    params.get(0).toJava(),
+                    functionNames,
+                    functionAssignments,
+                    this.rhs.toJava());
+        }
     }
 }
