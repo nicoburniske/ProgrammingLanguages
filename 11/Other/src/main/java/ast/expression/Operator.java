ae691369a619f1151a629a11492fe1559eda484b
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2020-04-03 13:37:45 -0400

    bherzberg adding operators, lets come back to them

---
+++ 11/Other/src/main/java/ast/expression/Operator.java
@@ -0,0 +1,46 @@
+package ast.expression;
+
+import org.json.simple.JSONArray;
+
+import java.util.Objects;
+
+public class Operator implements Expression {
+    private Expression lhs;
+    private Expression rhs;
+    private String op;
+
+    public Operator(Expression lhs, Expression rhs, String op) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.op = op;
+    }
+
+    @Override
+    public String toString() {
+        return this.toJSONString();
+    }
+
+    @Override
+    public String toJSONString() {
+        JSONArray arr = new JSONArray();
+        arr.add(lhs);
+        arr.add(op);
+        arr.add(rhs);
+        return arr.toJSONString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operator operator = (Operator) o;
+        return lhs.equals(operator.lhs) &&
+                rhs.equals(operator.rhs) &&
+                op.equals(operator.op);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lhs, rhs, op);
+    }
+}
